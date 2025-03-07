bf46a16539e1db1dba925b7822b00d6bb3642d6e
taherdoust <taherdoust@gmail.com>
2025-03-07 11:06:01 +0100

    Ensured index.html is in the root

--- weblog/views.py
+++ weblog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'weblog/home.html')
+
+def professional_weblog(request):
+    return render(request, 'weblog/professional.html')
+
+def social_weblog(request):
+    return render(request, 'weblog/social.html')
+
+def portfolio(request):
+    return render(request, 'weblog/portfolio.html')
+
+def personal_weblog(request):
+    return render(request, 'weblog/personal.html')
+
+def coaching(request):
+    return render(request, 'weblog/coaching.html')
